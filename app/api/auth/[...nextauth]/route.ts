28a86f3430521ba6e32ca6a7c023938304a0de60
Haruka Ogino <harukariq@gmail.com>
2024-06-06 13:47:54 +1200

    set up auth variables

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,12 +4,18 @@ import GoogleProvider from 'next-auth/providers/google'
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: '',
-      clientSecret: '',
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  async session({ session }) {
+    try {
+    } catch (error) {}
+  },
+  async signIn({ profile }) {
+    try {
+    } catch (error) {}
+  },
 })
 
 export { handler as GET, handler as POST }
