6d6f41fb73b901264cbd301183112d7c3849ef85
Haruka Ogino <harukariq@gmail.com>
2024-06-07 18:24:25 +1200

    create post types

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,4 +1,19 @@
-const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+import { Dispatch, SetStateAction } from 'react'
+
+interface Params {
+  type: string
+  post: Post
+  setPost: Dispatch<SetStateAction<Post>>
+  submitting: boolean
+  handleSubmit: (e: any) => Promise<void>
+}
+
+interface Post {
+  prompt: string
+  tag: string
+}
+
+const Form = ({ type, post, setPost, submitting, handleSubmit }: Params) => {
   return <section className="w-full flex-center flex-col"></section>
 }
 
