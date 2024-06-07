90acc0aa7478692f46b9c5c8bf0a7147e83f188f
Haruka Ogino <harukariq@gmail.com>
2024-06-07 15:18:04 +1200

    update profile image display

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -59,7 +59,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session.user.image ?? '/assets/images/logo.svg'}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -88,7 +88,7 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session.user.image ?? '/assets/images/logo.svg'}
               width={37}
               height={37}
               className="rounded-full"
