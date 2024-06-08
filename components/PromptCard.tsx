2f97947fcd8b4d991ff90d2aa0a6515d6f3dcd1b
Haruka Ogino <harukariq@gmail.com>
2024-06-08 13:15:53 +1200

    implement copy text fn

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -8,6 +8,12 @@ import { usePathname, useRouter } from 'next/navigation'
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState('')
 
+  function handleCopy() {
+    setCopied(post.prompt)
+    // line below copied to clipboard
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopied(''), 3000)
+  }
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
