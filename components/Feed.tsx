c03cce8c0060e5ed483b30140a7deb661164b6df
Haruka Ogino <harukariq@gmail.com>
2024-06-08 12:18:29 +1200

    populate feed component;

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,29 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import PromptCard from './PromptCard'
+
 const Feed = () => {
-  return <div className="">Feed</div>
+  const [searchText, setSearchText] = useState('')
+
+  function handleSearchChange(e) {}
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+        />
+      </form>
+
+      {/* component below will only be used inside this component so it can be created here */}
+      <PromptCardList data={[]} handleTagClick={() => {}} />
+    </section>
+  )
 }
 
 export default Feed
