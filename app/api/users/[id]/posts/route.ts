728473b036c5e4af15f007a3d7fcce2cfaff0f07
Haruka Ogino <harukariq@gmail.com>
2024-06-08 13:28:57 +1200

    create BE route for getting a user's prompts

---
+++ app/api/users/[id]/posts/route.ts
@@ -0,0 +1,16 @@
+import { connectToDB } from '@utils/database'
+import Prompt from '@models/prompt'
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher'
+
+export const Get = async (req: Request, { params }: Params) => {
+  try {
+    await connectToDB()
+    const prompts = await Prompt.find({ creator: params.id }).populate(
+      'creator'
+    )
+
+    return new Response(JSON.stringify(prompts), { status: 200 })
+  } catch (error) {
+    return new Response('Failed to fetch prompts', { status: 500 })
+  }
+}
