aa3ae8a7eb182233f71f477a89e182a0eab155e4
Haruka Ogino <harukariq@gmail.com>
2024-06-08 13:33:21 +1200

    set up profile component

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,5 +1,19 @@
-const Profile = () => {
-  return <section className="w-full flex-center flex-col"></section>
+import PromptCard from './PromptCard'
+
+interface Params {
+  name: string
+  desc: string
+  data: []
+  handleEdit: () => void
+  handleDelete: Promise<() => void>
+}
+
+const Profile = ({ name, desc, data, handleEdit, handleDelete }: Params) => {
+  return (
+    <section className="w-full">
+      <h1>{name} Profile</h1>
+    </section>
+  )
 }
 
 export default Profile
